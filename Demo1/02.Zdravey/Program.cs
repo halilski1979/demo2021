9df05f013f429aa7e96fb196e36f147a4997cb81
halilski1979 <halilski1979@gmail.com>
2021-01-29 19:05:11 +0200

    Hsdcds

---
+++ Demo1/02.Zdravey/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Zdravey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zdravey");
+        }
+    }
+}
